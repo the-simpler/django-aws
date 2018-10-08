b7a1ff5f8d58ce2f90b1db754fad2384d746844e
Yauheni Zhytkouski <y.zhytkouski@hackspace.by>
2018-10-09 01:16:02 +0300

    [ADD] Add generic views and templates

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
 
 # Register your models here.
